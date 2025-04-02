5c23cbdc3a9c93d609df5fb7e079be5927eb21a7
douglas moreira <douglas.cosmo7@gmail.com>
2025-04-02 15:41:14 -0300

    Commit inicial projeto

---
+++ src/main/java/com/example/cadastro_veiculo/CadastroVeiculoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cadastro_veiculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroVeiculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroVeiculoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cadastro_veiculo/CadastroVeiculoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cadastro_veiculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroVeiculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
