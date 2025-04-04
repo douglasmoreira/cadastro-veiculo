9fe670be24c600946232aaf73d212dc33d0b9fad
douglas moreira <douglas.cosmo7@gmail.com>
2025-04-04 08:02:03 -0300

    criação banco do repository

---
+++ src/main/java/com/example/cadastro_veiculo/repository/CarRepository.java
@@ -0,0 +1,10 @@
+package com.example.cadastro_veiculo.repository;
+
+import com.example.cadastro_veiculo.domain.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long>, JpaSpecificationExecutor<Car> {
+}
